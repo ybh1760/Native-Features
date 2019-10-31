4610f85c86e2782bfa8eebe29b68064c781c75ba
Yang <ybh1760@gmail.com>
2019-10-31 22:35:46 +0900

    storing address

--- store/place-actions.js
+++ store/place-actions.js
@@ -1,12 +1,26 @@
 import * as FileSystem from 'expo-file-system'
 
+import ENV from '../env'
 import { insertPlace, fetchPlace } from '../helpers/db'
 
 export const SET_PLACES = 'SET_PLACES'
 export const ADD_PLACE = 'ADD_PLACE'
 
-export const addPlace = (title, image) => {
+export const addPlace = (title, image, location) => {
     return async dispatch => {
+        const response = await fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=${ENV.googleApiKey}`
+        )
+
+        if (!response.ok) {
+            throw new Error('Geocoding occured Errors')
+        }
+        const resData = response.json()
+        if (!resData.results) {
+            throw new Error('results of response is not existed')
+        }
+        const address = resData.results[0].formatted_address
+
         const fileName = image.split('/').pop()
         const newPath = FileSystem.documentDirectory + fileName
 
@@ -18,9 +32,9 @@ export const addPlace = (title, image) => {
             const dbResult = await insertPlace(
                 title,
                 newPath,
-                'no address',
-                15.6,
-                12.4
+                address,
+                location.lat,
+                location.lng
             )
 
             dispatch({
@@ -29,6 +43,11 @@ export const addPlace = (title, image) => {
                     id: dbResult.insertId,
                     title: title,
                     image: newPath,
+                    address: address,
+                    coords: {
+                        lat: location.lat,
+                        lng: location.lng,
+                    },
                 },
             })
         } catch (err) {
